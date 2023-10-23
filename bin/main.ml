be8ce5a7eb4587dd869a4542eb2a615d3c7e637b
youkwhd <lolywk@tutanota.com>
2023-10-23 11:14:20 +0700

    feat: evaluate checking boolean value

--- bin/main.ml
+++ bin/main.ml
@@ -2,5 +2,7 @@
 let () =
   let ht = Hashtbl.create 10 in
   Hashtbl.add ht "name" "Jackalope";
+  (* I thought Hashtbl has any type *)
+  Hashtbl.add ht "isok" "__true";
 
-  List.iter print_string (Mustacml.parse (Mustacml.generate_from_string "Hello {{name}}") ht)
+  List.iter print_string (Mustacml.parse (Mustacml.generate_from_string "Hello {{name}} {{#isok}}what the fuck{{/isok}}") ht)
--- lib/parser.ml
+++ lib/parser.ml
@@ -3,24 +3,57 @@ include Lexer
 let todo _info =
   failwith _info
 
-let rec parse tokens ht =
-  match tokens with
-  | [] -> []
-  | tok :: tokens ->
-    if tok.kind = MUSTACHE_VAR_ESCAPE_OPEN then
-      match tokens with
-      | [] -> failwith "EXPECTED VARIABLE NAME"
-      | tok :: tokens -> 
-        match tok.kind with
-        | HASH_SIGN -> todo "TODO: Check if truthy value is true"
-        | CARET -> todo "TODO: Check if truthy value is not true"
-        | SLASH -> todo "TODO: Keep track if this is the end of section"
-        | TEXT -> 
-          let text = Hashtbl.find ht tok.text in
-          let next_tokens = match tokens with
-          | [] -> failwith "EXPECTED VARIABLE CLOSER"
-          | tok :: tokens -> if tok.kind = MUSTACHE_VAR_ESCAPE_CLOSE then tokens else failwith "EXPECTED VARIABLE CLOSER" in
-          text :: parse next_tokens ht
-        | _ -> failwith "EXPECTED VARIABLE NAME, FOUND := ?"
-    else
-      tok.text :: parse tokens ht
+let parse tokens ht =
+  let rec aux tokens ht stack =
+    match tokens with
+    | [] ->
+      if not (Stack.is_empty stack) then
+        failwith "NOT EMPTY"
+      else
+        []
+    | tok :: tokens ->
+      if tok.kind = MUSTACHE_VAR_ESCAPE_OPEN then
+        match tokens with
+        | [] -> failwith "EXPECTED VARIABLE NAME"
+        | tok :: tokens -> 
+          match tok.kind with
+          | HASH_SIGN -> 
+            (match tokens with
+             | [] -> failwith "EXPECTED VARIABLE NAME"
+             | tok :: tokens ->
+              match Hashtbl.find_opt ht tok.text with
+              (* Boolean value *)
+              | Some _ ->
+                let next_tokens = match tokens with
+                | [] -> failwith "EXPECTED VARIABLE CLOSER"
+                | tok :: tokens -> if tok.kind = MUSTACHE_VAR_ESCAPE_CLOSE then tokens else failwith "EXPECTED VARIABLE CLOSER" in
+                Stack.push tok.text stack;
+                aux next_tokens ht stack
+              | None -> failwith "q")
+          | CARET -> todo "TODO: Check if truthy value is not true"
+          | SLASH -> 
+              (match tokens with
+               | [] -> failwith "EXPECTED VARIABLE CLOSER"
+               | tok :: tokens ->
+                   if Stack.pop stack <> tok.text then
+                      failwith "EXPECTED SECTION CLOSER () NOT FOUND"
+                   else
+                      (match tokens with
+                      | [] -> failwith "EXPECTED ESCAPE CLOSER"
+                      | tok :: tokens ->
+                          if tok.kind = MUSTACHE_VAR_ESCAPE_CLOSE then
+                            aux tokens ht stack
+                          else
+                            failwith "EXPECTED VARIABLE CLOSER"))
+          | TEXT -> 
+            let text = Hashtbl.find ht tok.text in
+            let next_tokens = match tokens with
+            | [] -> failwith "EXPECTED VARIABLE CLOSER"
+            | tok :: tokens -> if tok.kind = MUSTACHE_VAR_ESCAPE_CLOSE then tokens else failwith "EXPECTED VARIABLE CLOSER" in
+            text :: aux next_tokens ht stack
+          | _ -> failwith "EXPECTED VARIABLE NAME, FOUND := ?"
+      else
+        tok.text :: aux tokens ht stack
+  in
+
+  aux tokens ht (Stack.create ())
